2fd47cae538fbb748cf7af6d35e6f4e608cfc627
Diego Andrés Robles Monge <diego.anrm07@gmail.com>
2024-10-18 01:28:07 -0600

    Creacion del proyecto y primero componente
    Se creo el proyecto, recordar agregar bootswatch y node, ademas ya esta creado el primero componente main

---
+++ PetSyncReact/src/componentes/MainFeed.jsx
@@ -0,0 +1,29 @@
+
+export const MainFeed = () => {
+  return (
+    <>
+    <div className="container">
+
+
+    <div>
+      <button className="btn btn-outline-warning btn-lg selectorsMain">Para ti</button>
+      <button className="btn btn-outline-warning btn-lg selectorsMain">Siguiendo</button>
+    </div>
+    <dir>
+      <div className="div newPostInput_Div">
+        <img src="" alt="" /><input placeholder="Cuentanos sobre las ultimas noticias de tu mascota!
+" className="input_NewPost" type="text" />
+      </div>
+      <div className="div newPostButtom_Div">
+        <button className="btn btn-warning">Subir</button>
+      </div>
+    </dir>
+    <hr />
+
+
+    </div>
+    
+    
+    </>
+  )
+}
---
+++ PetSyncReact/src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import 'bootswatch/dist/zephyr/bootstrap.min.css'
+import { MainFeed } from './componentes/MainFeed.jsx'
+import "../src/estilos/stylesheetMainFeed.css"
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+  <MainFeed></MainFeed>
+
+  </StrictMode>,
+)
