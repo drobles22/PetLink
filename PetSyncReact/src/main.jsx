cfbaa569e1d629d612c26cafdfaedbc2ce3d738b
Diego Andrés Robles Monge <diego.anrm07@gmail.com>
2024-10-19 04:11:59 -0600

    Estructura de main
    Terminar estructura de la pagina de main, faltan detalles

--- PetSyncReact/src/main.jsx
+++ PetSyncReact/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootswatch/dist/zephyr/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import { MainFeed } from './componentes/MainFeed.jsx'
 import "../src/estilos/stylesheetMainFeed.css"
 
