57b4a9a2844a6f0db823c3652fd7d32a741a8379
carmuco <111482440+carmuco@users.noreply.github.com>
2024-11-05 20:49:24 -0600

    Add files via upload

---
+++ PetSyncReact/src/componentes/LoginPage/Login.jsx
@@ -0,0 +1,27 @@
+import "/src/estilos/Login.css";
+
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">PetSync</h3>
+          <span className="loginDesc">
+            Conecta con aquellos amigos peludos al rededor del mundo!!.
+          </span>
+        </div>
+        <div className="loginRight">
+          <div className="loginBox">
+            <input placeholder="Email" className="loginInput" />
+            <input placeholder="Password" className="loginInput" />
+            <button className="loginButton">Log In</button>
+            <span className="loginForgot">Forgot Password?</span>
+            <button className="loginRegisterButton">
+              Create a New Account
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
