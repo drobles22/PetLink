d59318dbcbbdc0da138049a24e7d7275d6ac88f9
carmuco <111482440+carmuco@users.noreply.github.com>
2024-12-17 19:38:16 -0600

    MainFeed.jsx

--- PetSyncReact/src/componentes/MainPage/MainFeed.jsx
+++ PetSyncReact/src/componentes/MainPage/MainFeed.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
-import PropTypes from "prop-types"; 
+import PropTypes from "prop-types"; // Importar PropTypes
 import { Post } from "./Post";
-import { Share } from "./Share";
+import { Share } from "./Share"; // Importa el componente Share
 import axios from "axios";
 import "../../estilos/stylesheetMainFeed.css";
 
@@ -13,29 +13,33 @@ export const MainFeed = ({ username }) => {
       try {
         const response = username
           ? await axios.get(`/api/posts/profile/${username}`)
-          : await axios.get("api/posts/timeline/6712fdbba4c3191dee6e3539");
-  
+          : await axios.get("/api/posts/timeline/6712fdbba4c3191dee6e3539");
+
         console.log("Fetched posts:", response.data);
         setPosts(response.data);
       } catch (error) {
         console.error("Error tomando los posts:", error);
       }
     };
-  
+
     fetchPost();
   }, [username]);
+
   return (
     <div className="container feedcontainer">
-      {/* Llamar al componente Share */}
+      {/* Componente Share */}
       <Share />
+
+      {/* Muestra las publicaciones */}
       {posts.map((p) => (
         <Post key={p._id} post={p} />
       ))}
+
       <hr />
     </div>
   );
 };
 
 MainFeed.propTypes = {
-  username: PropTypes.string
-}
+  username: PropTypes.string,
+};
