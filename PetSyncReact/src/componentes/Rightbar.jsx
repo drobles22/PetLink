fbcbd5b7c0ecbacb42b3bd0c79d1ff10b34faf50
Pablo <josepablosanchezsoto@gmail.com>
2024-12-19 00:46:23 -0600

    RIGTHBAR
    COMIT

--- PetSyncReact/src/componentes/Rightbar.jsx
+++ PetSyncReact/src/componentes/Rightbar.jsx
@@ -1,30 +1,12 @@
-/* eslint-disable no-empty */
 import "../estilos/rightbar.css";
 import Online from "../componentes/Online";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import { NavLink } from "react-router-dom";
-import { AuthContext } from "../context/AuthContext";
-
-const Users = [
-  {
-    id: 1,
-    profilePicture: "/path/to/profile1.jpg",
-    username: "User1",
-  },
-  {
-    id: 2,
-    profilePicture: "/path/to/profile2.jpg",
-    username: "User2",
-  },
-  // Add more users as needed
-];
+import { Link } from "react-router-dom";
 
 export default function Rightbar() {
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+  const PF = "/";
   const [friends, setFriends] = useState([]);
-  const { user: currentUser, dispatch } = useContext(AuthContext);
-  const [followed, setFollowed] = useState( currentUser.followings.includes(user?.id) );
 
   useEffect(() => {
     const getFriends = async () => {
@@ -38,38 +20,11 @@ export default function Rightbar() {
     getFriends();
   }, [user]);
 
-  const handleClick = async () => {
-
-    try {
-      if (followed) {
-        await axios.put(`/users/${user._id}/unfollow`, {
-          userId: currentUser._id,
-        });
-        dispatch({ type: "UNFOLLOW", payload: user._id });
-      } else {
-        await axios.put(`/users/${user._id}/follow`, {
-          userId: currentUser._id,
-        });
-        dispatch({ type: "FOLLOW", payload: user._id });
-      }
-      setFollowed(!followed);
-          // eslint-disable-next-line no-unused-vars
-    } catch (err) {
-
-    }
-  };
-
   function HomeRightbar() {
     return (
       <>
-        <div className="birthdayContainer">
-          <img className="birthdayImg" src="assets/gift.png" alt="" />
-          <span className="birthdayText">
-            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
-          </span>
-        </div>
         <img className="rightbarAd" src="assets/ad.png" alt="" />
-        <h4 className="rightbarTitle">Online Friends</h4>
+        <h4 className="rightbarTitle">Friends</h4>
         <ul className="rightbarFriendList">
           {Users.map((u) => (
             <Online key={u.id} user={u} />
@@ -79,62 +34,32 @@ export default function Rightbar() {
     );
   }
   
-  const ProfileRightbar = () => {
+  function ProfileRightbar() {
     return (
       <>
-        {user.username !== currentUser.username && (
-          <button className="rightbarFollowButton" onClick={handleClick}>
-            {followed ? "Unfollow" : "Follow"}
-            {/*followed ? <Remove /> : <Add />*/}
-          </button>
-        )}
-        <h4 className="rightbarTitle">User information</h4>
-        <div className="rightbarInfo">
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">City:</span>
-            <span className="rightbarInfoValue">{user.city}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">From:</span>
-            <span className="rightbarInfoValue">{user.from}</span>
-          </div>
-          <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">Relationship:</span>
-            <span className="rightbarInfoValue">
-              {user.relationship === 1
-                ? "Single"
-                : user.relationship === 1
-                ? "Married"
-                : "-"}
-            </span>
-          </div>
-        </div>
         <h4 className="rightbarTitle">User friends</h4>
         <div className="rightbarFollowings">
           {friends.map((friend) => (
-            //eslint-disable-next-line react/jsx-key
-            <NavLink
-              to={"/profile/" + friend.username}
+            // eslint-disable-next-line react/jsx-key
+            <Link
+              to={`/profile/${friend.username}`}
               style={{ textDecoration: "none" }}
             >
               <div className="rightbarFollowing">
                 <img
-                  src={
-                    friend.profilePicture
-                      ? PF + friend.profilePicture
-                      : PF + "person/noAvatar.png"
-                  }
+                  src={friend.profilePicture
+                    ? PF + friend.profilePicture
+                    : PF + "person/noAvatar.png"}
                   alt=""
-                  className="rightbarFollowingImg"
-                />
+                  className="rightbarFollowingImg" />
                 <span className="rightbarFollowingName">{friend.username}</span>
               </div>
-            </NavLink>
+            </Link>
           ))}
         </div>
       </>
     );
-  };
+  }
 
   return (
     <div className="rightbar">
